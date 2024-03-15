f21dee32f86fbc1c367f959fd4bb890e498cc612
victorDGW <victor-de-germain_student2022@wilder.school>
2024-03-15 13:44:59 +0100

    fix config jest to ignore file and force test

--- tests/utils/trueTest.spec.ts
+++ tests/utils/trueTest.spec.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from '@jest/globals'
 
 describe('UTILS functions', () => {
-  it('should return true if length of code is correct', async () => {
+  it('should return true in all test', async () => {
     expect(true).toBe(true)
   })
 })
