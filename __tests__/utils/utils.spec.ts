b92426aac70d4042f4c0904fbc0c252c269fe4b1
victorDGW <victor-de-germain_student2022@wilder.school>
2024-03-15 11:29:48 +0100

    config gitaction push and pr on main

--- __tests__/utils/utils.spec.ts
+++ __tests__/utils/utils.spec.ts
@@ -1,23 +1,25 @@
-import 'reflect-metadata';
-import { config } from "dotenv";
-config();
-import { describe, it, expect } from "@jest/globals";
-import { generateSecurityCode } from "../../src/utils/utils";
+import 'reflect-metadata'
+import { config } from 'dotenv'
+config()
+import { describe, it, expect } from '@jest/globals'
+import { generateSecurityCode } from '../../src/utils/utils'
 
-describe("UTILS functions", () => {
-  it("should return true if length of code is correct", async () => {
-    const length = 10;
-    const randomCode = generateSecurityCode(length);
-    
-    expect(typeof randomCode).toBe("string");
-    expect(randomCode).toHaveLength(length);
-  });
+describe('UTILS functions', () => {
+  it('should return true if length of code is correct', async () => {
+    const length = 10
 
-  it("should return false if length of code is NOT correct", async () => {
-    const length = 10;
-    const randomCode = generateSecurityCode(length);
-    
-    expect(typeof randomCode).toBe("string");
-    expect(randomCode).not.toHaveLength(length-1);
-  });
-});
+    const a = 10
+    const randomCode = generateSecurityCode(length)
+
+    expect(typeof randomCode).toBe('string')
+    expect(randomCode).toHaveLength(length)
+  })
+
+  it('should return false if length of code is NOT correct', async () => {
+    const length = 10
+    const randomCode = generateSecurityCode(length)
+
+    expect(typeof randomCode).toBe('string')
+    expect(randomCode).not.toHaveLength(length - 1)
+  })
+})
