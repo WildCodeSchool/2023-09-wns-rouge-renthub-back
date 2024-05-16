cac576edb4807ea001f54698a72c40d3ce797f87
HugoDvdTApp <hugo@toolapp.fr>
2024-05-16 12:08:57 +0200

    MàJ resolvers in progress: user done

--- src/resolvers/Carts.resolver.ts
+++ src/resolvers/Carts.resolver.ts
@@ -1,22 +1,34 @@
-import { Arg, ID, Mutation, Query, Resolver } from 'type-graphql'
-
+import {
+  Arg,
+  Authorized,
+  Ctx,
+  ID,
+  Mutation,
+  Query,
+  Resolver,
+} from 'type-graphql'
 import { Cart, CartUpdateInput } from '../entities/Cart.entity'
 import { CartService } from '../services/Cart.service'
+import { MyContext } from '../types/Context.type'
 
 @Resolver(() => Cart)
 export class CartResolver {
+  @Authorized('ADMIN')
   @Query(() => [Cart])
   async findAllCarts(): Promise<Cart[]> {
     const carts = await new CartService().findAll()
     return carts
   }
 
+  @Authorized('ADMIN', 'USER')
   @Query(() => Cart)
-  async findCart(@Arg('id', () => ID) id: number) {
+  async findCart(@Arg('id', () => ID) id: number, @Ctx() context: MyContext) {
+    console.log(context.user)
     const cart = await new CartService().find(+id)
     return cart
   }
 
+  @Authorized('ADMIN', 'USER')
   @Mutation(() => Cart, { nullable: true })
   async updateCart(
     @Arg('id', () => ID) id: number,
